b006802ea5ede4d0b03e6e2d22a795c51b2e17d6
Vyacheslav <vyacheslav.tsvetkov.sar@gmail.com>
2026-03-24 01:30:56 +0300

    A registration page has been added, along with a function for handling POST requests in Django

--- artists_card/urls.py
+++ artists_card/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('artist/<int:artist_id>/', views.artist_detail, name='artist_detail'),
+    path('auth/', views.submit_data, name='auth'),
 ]
--- artists_card/views.py
+++ artists_card/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Artist_page
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def artist_detail(request, artist_id):
     artist = get_object_or_404(Artist_page, id=artist_id)
     return render(request, 'artist_page.html', {'artist': artist})
+
+@csrf_exempt
+def submit_data(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Данные получены и обработаны'
+        })
+    return render(request, 'auth_form.html')
